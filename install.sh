286696bd2e579d1ce7ba5a3e214d6e1da9e289ad
BlueDragon <51781584+kalibrado@users.noreply.github.com>
2024-01-25 13:19:20 +0000

    update

--- install.sh
+++ install.sh
@@ -1,18 +1,6 @@
 import os
 import subprocess
 
-if 'which code'.strip():
-    code_extensions = sorted(subprocess.getoutput('cat code_extensions').splitlines())
-    installed_extensions = sorted(subprocess.getoutput('code-server --list-extensions').splitlines())
-    uninstalled_extensions = list(set(code_extensions) - set(installed_extensions))
-    print('Checking for uninstalled VSCode extensions...')
-    if not uninstalled_extensions:
-        print("all good!\r\n")
-    else:
-        print(f"found {len(uninstalled_extensions)}.\r\n")
-        for extension in uninstalled_extensions:
-            print(f"Installing {extension}...")#!/bin/bash
-
 if command -v code-server &> /dev/null; then
     code_extensions=$(sort < code_extensions)
     installed_extensions=$(code-server --list-extensions | sort)
