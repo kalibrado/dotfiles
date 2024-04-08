147f705d947fd278e7d495c99ef10c5b06d0f404
BlueDragon <51781584+kalibrado@users.noreply.github.com>
2024-04-08 13:47:17 +0000

    bashrc

--- install.sh
+++ install.sh
@@ -3,31 +3,19 @@
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
-########## Variables
+echo "Install ZSH"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc bash_aliases"                    # list of files/folders to symlink in homedir
 
-##########
 
-# create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
-echo "done"
 
-# change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
-cd $dir
-echo "done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
-done
+
+echo "Creating symlink to bash folder in home directory."
+ln -s $dir/bash/* ~/.$file
+
+echo "Creating symlink to zsh folder in home directory."
+ln -s $dir/zsh/* ~/.$file
 
 # Visual Studio Code :: Package list
 pkglist=(
